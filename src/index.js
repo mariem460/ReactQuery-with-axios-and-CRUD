e1d4757dbfbf111c91bdb44bb7ebe8fb5ba31478
mariem460 <mariem.oueslati.hajjem@gmail.com>
2023-01-20 11:42:14 +0100

    install reactQuery and axios

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClientProvider, QueryClient} from '@tanstack/react-query'; 
+import {ReactQueryDevtools} from 'react-query/devtools' ;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient()
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsopen/>
+
+    </QueryClientProvider>
+ 
   </React.StrictMode>
 );
 
